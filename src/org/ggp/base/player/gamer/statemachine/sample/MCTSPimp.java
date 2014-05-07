8fcdc51fa24d5bdeddc1d8b79197c05008426269
drokia2 <drokia2@gmail.com>
2014-05-06 19:00:11 -0700

    constant

--- src/org/ggp/base/player/gamer/statemachine/sample/MCTSPimp.java
+++ src/org/ggp/base/player/gamer/statemachine/sample/MCTSPimp.java
@@ -30,10 +30,12 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 public final class MCTSPimp extends SampleGamer
 {
 
-	private static final int SELECT_CONST = 1;
+	private static final int SELECT_CONST = 10;
 	private static final int NUM_CHARGES = 1;
 	private static final int MC_TIMEOUT_MARGIN = 100;
 	private static final int SHORT_TIMEOUT_MARGIN = 30;
+	private Map<MachineState, Integer> numVisits;
+	private Map<MachineState, Integer> totals;
 	/**
 	 * Employs a simple sample "Monte Carlo" algorithm.
 	 */
@@ -42,7 +44,6 @@ public final class MCTSPimp extends SampleGamer
 	{
 	    StateMachine theMachine = getStateMachine();
 		long start = System.currentTimeMillis();
-		long finishBy = timeout - 1000;
 
 		List<Move> moves = theMachine.getLegalMoves(getCurrentState(), getRole());
 		Move selection = doTheMonteCarlo(getRole(), getCurrentState(), timeout);
@@ -55,36 +56,35 @@ public final class MCTSPimp extends SampleGamer
 
 
 	private int[] depth = new int[1];
-
 	private Move doTheMonteCarlo(Role role, MachineState currentState,
 			long timeout) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
 
 		StateMachine SM = getStateMachine();
-		Map<MachineState, Integer> numVisits = new HashMap<MachineState, Integer>();
-		Map<MachineState, Integer> totals = new HashMap<MachineState, Integer>();
-		MachineState selectedState = currentState;
-		numVisits.put(selectedState, 0);
-		totals.put(selectedState, 0);
+		numVisits = new HashMap<MachineState, Integer>();
+		totals = new HashMap<MachineState, Integer>();
+		numVisits.put(currentState, 0);
+		totals.put(currentState, 0);
+
 		while(true) {
 			if (timeout - System.currentTimeMillis() <= MC_TIMEOUT_MARGIN){
 				break;
 			}
 			ArrayList<MachineState> path = new ArrayList<MachineState>();//for backprop
-			selectedState = select(numVisits, totals, selectedState, role, path, timeout);
+			MachineState selectedState = select(currentState, role, path, timeout);
 
 			if (SM.isTerminal(selectedState))  {
 				numVisits.put(selectedState, numVisits.get(selectedState) + 1);
 				totals.put(selectedState, totals.get(selectedState) +SM.getGoal(selectedState, role));
 				continue;
 			}
-			expand(selectedState, numVisits, totals, role);
+			expand(selectedState, role);
 			//TODO: maybe do it more than once
 			int dcScore = 0;
 			for (int i=0; i< NUM_CHARGES; i++) {
 				dcScore += SM.getGoal(SM.performDepthCharge(selectedState, depth), role);
 			}
 
-			backpropagate(selectedState, dcScore, path, numVisits, totals, role);
+			backpropagate(selectedState, dcScore, path, role);
 
 
 
@@ -97,6 +97,9 @@ public final class MCTSPimp extends SampleGamer
 			List<List<Move>> legalJointMoves = SM.getLegalJointMoves(currentState, role, legalMoves.get(i));
 			for(int j=0; j< legalJointMoves.size(); j++) {
 				MachineState child = SM.getNextState(currentState, legalJointMoves.get(j));
+				if (numVisits.get(child) == 0) {
+					continue;
+				}
 				double childUtility = totals.get(child)/(double)numVisits.get(child);
 				if(childUtility > bestUtility) {
 					bestUtility = childUtility;
@@ -116,8 +119,7 @@ public final class MCTSPimp extends SampleGamer
 
 
 	private void backpropagate(MachineState selectedState, int dcScore,
-			ArrayList<MachineState> path, Map<MachineState, Integer> numVisits,
-			Map<MachineState, Integer> totals, Role role) throws GoalDefinitionException {
+			ArrayList<MachineState> path, Role role) throws GoalDefinitionException {
 		StateMachine SM = getStateMachine();
 		for(int i=0; i< path.size(); i++) {
 			MachineState cur = path.get(i);
@@ -132,9 +134,7 @@ public final class MCTSPimp extends SampleGamer
     /*
      * Expand simply adds the children of the selected node to numVisits and totals
      */
-	private void expand(MachineState state,
-			Map<MachineState, Integer> numVisits,
-			Map<MachineState, Integer> totals, Role role) throws MoveDefinitionException, TransitionDefinitionException {
+	private void expand(MachineState state, Role role) throws MoveDefinitionException, TransitionDefinitionException {
 		StateMachine SM = getStateMachine();
 		List<Move> legalMoves = SM.getLegalMoves(state, role);
 		for (int i=0; i < legalMoves.size(); i++) {
@@ -151,8 +151,7 @@ public final class MCTSPimp extends SampleGamer
 
 
 
-	private MachineState select(Map<MachineState, Integer> numVisits,
-			Map<MachineState, Integer> totals, MachineState state, Role role, ArrayList<MachineState> path, long timeout) throws MoveDefinitionException, TransitionDefinitionException {
+	private MachineState select(MachineState state, Role role, ArrayList<MachineState> path, long timeout) throws MoveDefinitionException, TransitionDefinitionException {
 		StateMachine SM = getStateMachine();
 		while (true) {
 			path.add(state);
@@ -184,7 +183,7 @@ public final class MCTSPimp extends SampleGamer
 					if(numVisits.get(child) == null) {
 						continue;
 					}
-					int curScore = selectFn(state, child, numVisits, totals);
+					int curScore = selectFn(state, child);
 					if (curScore> score) {
 						score = curScore;
 						result = child;
@@ -197,10 +196,11 @@ public final class MCTSPimp extends SampleGamer
 
 
 
-	private int selectFn(MachineState parent, MachineState child,
-			Map<MachineState, Integer> numVisits,
-			Map<MachineState, Integer> totals) {
+	private int selectFn(MachineState parent, MachineState child) {
 		double utility = totals.get(child)/ (double)numVisits.get(child);// avg reward
+		if (numVisits.get(child) == 0) {
+			System.out.println("its zero bitch");
+		}
 		return (int) (utility + SELECT_CONST *Math.sqrt(2*Math.log(numVisits.get(parent))/numVisits.get(child)));
 	}
 
